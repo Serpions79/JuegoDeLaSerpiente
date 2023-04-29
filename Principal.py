d1e6c2c6262171ac3ba8ff3b625049c3133cb1e2
Serpions79 <randommolten007@gmail.com>
2023-04-29 14:02:49 +0200

    Primera decision

--- Principal.py
+++ Principal.py
@@ -6,9 +6,21 @@ print("Hace mucho que no hablamos tu y yo")
 print("La ultima vez fue cuando tenias como 4 años")
 print("Hace tanto que hasta se me olvido tu nombre")
 
-Nombre_del_personaje=int(input())
-print("Es verdad cuanto tiempo" Nombre_del_personaje "Espero que estes tan bien como estoy yo")
-print(Nombre_del_personaje "Si estoy aqui hablando contigo es que algo malo ha ocurrido recuerda lo que nos dijo el tio Ben")
+Nombre_del_personaje=input()
+print("Es verdad cuanto tiempo " +  Nombre_del_personaje + ", espero que estes tan bien como estoy yo")
+print(Nombre_del_personaje + ", si estoy aqui hablando contigo es que algo malo ha ocurrido recuerda lo que nos dijo el tio Ben")
 print("Si comes lechugas te convertiras en un superheroe")
 print("A espera eso no")
 print("Si escuchas una voz en tu cabeza debes de ir a mi antigua casa situada en la Isla Mauricio lo entenderas cuando sea el momento idoneo")
+print("Entonces...")
+print("Ya sabemos que hacer...por asi decirlo")
+print("Oye yo soy tu consciencia y como tal seguramente hare unas preguntas en el viaje")
+print("No hay problema no?")
+print("ESCOGE 1 PARA DECIR SÍ ESCOGE 2 PARA DECIR NO (PUDRETE)")
+decision=int(input())
+if decision==1:
+    print("Gracias por entenderme intentare no molestarte mucho en tu cabeza")
+elif decision==2:
+    print("Perdon...")
+
+
